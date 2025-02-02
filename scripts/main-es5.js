5169f621b52f3420b2239ef7ff97616b64279a77
oatmeeru <demonicoatmeal@gmail.com>
2025-02-01 23:41:15 -0800

    itty bitty octobaby

--- scripts/main-es5.js
+++ scripts/main-es5.js
@@ -1,19 +1,19 @@
 /* main-es5.js */
 
 // look this up :)
-'use strict';
+// 'use strict';
 
-function updateName() {
-    var name = document.getElementById('name-input').value;
-    document.getElementById('author-info').innerHTML = '<strong>Author:</strong> ' + name;
-  }
-  
-  function updateMajor() {
-    var name = document.getElementById('major-input').value;
-    document.getElementById('major-info').innerHTML = '<strong>Major:</strong> ' + name;
-  }
-  
-function update(){
-    updateName();
-    updateMajor();
-  }
+// function updateName() {
+//     var name = document.getElementById('name-input').value;
+//     document.getElementById('author-info').innerHTML = '<strong>Author:</strong> ' + name;
+//   }
+
+//   function updateMajor() {
+//     var name = document.getElementById('major-input').value;
+//     document.getElementById('major-info').innerHTML = '<strong>Major:</strong> ' + name;
+//   }
+
+// function update(){
+//     updateName();
+//     updateMajor();
+//   }
